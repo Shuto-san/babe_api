aa3dbfbe2d8b2725703ef5c4e5e16d1487db5ade
shuto-san <shuemon2014@outlook.jp>
2020-01-19 19:02:37 +0900

    migration files: create tables

---
+++ db/migrations/000004_create_reviews_table.up.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `reviews` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id_from` bigint(20) unsigned NOT NULL,
+  `user_id_to` bigint(20) unsigned NOT NULL,
+  `rating` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `comment` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `answer` json DEFAULT NULL,
+  `agree_count` int(10) unsigned NOT NULL DEFAULT '0',
+  `disagree_count` int(10) unsigned NOT NULL DEFAULT '0',
+  `status` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+  KEY `user_id_multi_index` (`user_id_from`, `user_id_to`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
